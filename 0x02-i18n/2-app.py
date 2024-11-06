8a862ef8a574709b39a83cf9750b35bd654f1788
VectorGits <davidoluremi0@gmail.com>
2024-11-06 18:17:57 +0100

    Fix decorator syntax for locale selector in Flask app

--- 0x02-i18n/2-app.py
+++ 0x02-i18n/2-app.py
@@ -17,7 +17,7 @@ class Config:
 app.config.from_object(Config)
 
 
-babel.localeselector
+@babel.localeselector
 def get_locale():
     """Get locale from request"""
     return request.accept_languages.best_match(app.config['LANGUAGES'])
