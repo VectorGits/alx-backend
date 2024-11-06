67ff46c5a25f67f214beb48aa879e4b3c16aadb2
VectorGits <davidoluremi0@gmail.com>
2024-11-06 18:15:32 +0100

    Add Babel setup and initial HTML template for internationalization

--- 0x02-i18n/2-app.py
+++ 0x02-i18n/2-app.py
@@ -17,6 +17,12 @@ class Config:
 app.config.from_object(Config)
 
 
+babel.localeselector
+def get_locale():
+    """Get locale from request"""
+    return request.accept_languages.best_match(app.config['LANGUAGES'])
+
+
 @app.route('/', strict_slashes=False)
 def index():
     """Render index.html template"""
