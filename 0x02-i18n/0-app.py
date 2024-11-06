01979233350554b260080bc5881109d8570af368
VectorGits <davidoluremi0@gmail.com>
2024-11-06 17:52:26 +0100

    Add documentation

--- 0x02-i18n/0-app.py
+++ 0x02-i18n/0-app.py
@@ -1,4 +1,5 @@
 #!/usr/bin/env python3
+"""Basic Babel setup"""
 from flask import Flask, render_template
 
 app = Flask(__name__)
@@ -6,6 +7,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def index():
+    """Render index.html template"""
     return render_template('0-index.html')
 
 
